1a7f53fe63162bc2259f6d6ccabc57cff98169a8
Rockncoder <rockncoder@gmail.com>
2025-07-28 19:39:05 -0700

    01_01 - KMP Project Setup with Ktor Server

--- server/src/main/kotlin/com/kmp/explore/plugins/Serialization.kt
+++ server/src/main/kotlin/com/kmp/explore/plugins/Serialization.kt
@@ -1,16 +1,7 @@
 package com.kmp.explore.plugins
 
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
-import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
-    install(ContentNegotiation) {
-        json(Json {
-            prettyPrint = true
-            isLenient = true
-            ignoreUnknownKeys = true
-        })
-    }
+    // Serialization configuration will be added in later modules
 }
